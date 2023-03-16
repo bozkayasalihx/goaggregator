142220339a542aa0e4efa7208aea0590a101abe6
PF <pf@PFs-MacBook-Air.local>
2023-03-16 19:52:47 +0300

    removed some hard-coded strings

--- tester/test.go
+++ tester/test.go
@@ -10,12 +10,13 @@ import (
 
 func RunCollection() (context.Context, *mongo.Database) {
 	var MONGO_URL string = os.Getenv("MONGO_TEST_URL")
+	dbName ;= os.Getenv("DATABASE")
 	ctx := context.Background()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MONGO_URL))
 	if err != nil {
 		panic(err)
 	}
 
-	db := client.Database("gearbox-insight")
+	db := client.Database(dbName)
 	return ctx, db
 }
